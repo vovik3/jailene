40063a4b8973c8fc392aa78517c21ffff87d3e45
Vovik <voviknull@gmail.com>
2017-04-26 22:48:41 -0400

    Update settings.php

--- settings.php
+++ settings.php
@@ -7,7 +7,7 @@ header("location:index.php");
 $username = $_SESSION['username'];
 ?>
 
-<?php include ('header.php'); ?> 
+<?php include ('jailene/header.php'); ?> 
 
 <?php
 $update = $_GET['update'];
@@ -108,4 +108,4 @@ $gender = $match_value['gender'];
     return confirm("Are you sure you want to Logout?");
 })
 </script>
-<?php include ('footer.php'); ?> 
+<?php include ('jailene/footer.php'); ?> 
