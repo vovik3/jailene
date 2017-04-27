86da10191f9d9daa840c88393c7876ba1b649a66
Vovik <voviknull@gmail.com>
2017-04-26 22:42:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include ('header.php'); ?> 
+<?php include ('jailene/header.php'); ?> 
 
 
       <div class="masthead">
@@ -49,4 +49,4 @@
 
 
 ?
-<?php include ('footer.php'); ?> 
+<?php include ('jailene/footer.php'); ?> 
